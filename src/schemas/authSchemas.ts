6b32c53f4542b95cf44860e807f4a2848e9417f4
SantiGVadone <santiagovadone36@gmail.com>
2026-08-21 17:05:11 -0300

    feat: add changePasswordSchema and refreshSchema

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -12,3 +12,16 @@ export const loginSchema = z.object({
   email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters long')
 })
+
+export const changePasswordSchema = z.object({
+  oldPassword: z
+    .string()
+    .min(6, 'Old password must be at least 6 characters long'),
+  newPassword: z
+    .string()
+    .min(6, 'New password must be at least 6 characters long')
+})
+
+export const refreshSchema = z.object({
+  refreshToken: z.string().min(1, 'Refresh token is required')
+})

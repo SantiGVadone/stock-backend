5e14d6a7ef55bc26049e2e70b900217905389964
SantiGVadone <santiagovadone36@gmail.com>
2026-08-21 17:06:16 -0300

    feat: add changepassword and refresh routes

--- src/routes/validate.ts
+++ src/routes/validate.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express'
 import { validateBody } from '../middleware/validateMiddleware'
-import { registerSchema, loginSchema } from '../schemas/authSchemas'
+import {
+  registerSchema,
+  loginSchema,
+  changePasswordSchema,
+  refreshSchema
+} from '../schemas/authSchemas'
 import * as AuthController from '../controllers/authControllers'
 
 const router = Router()
@@ -8,5 +13,13 @@ const router = Router()
 router.post('/register', validateBody(registerSchema), AuthController.register)
 
 router.post('/login', validateBody(loginSchema), AuthController.login)
+
+router.post(
+  '/change-password',
+  validateBody(changePasswordSchema),
+  AuthController.changePassword
+)
+
+router.post('/refresh', validateBody(refreshSchema), AuthController.refresh)
 
 export default router

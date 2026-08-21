440d0804eeac6d489ab5a9ce67ae73e26321221b
SantiGVadone <santiagovadone36@gmail.com>
2026-08-21 17:09:40 -0300

    fix: add refresh controllers, changePassword controllers, update login controllers to use refreshtokens

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,7 +1,11 @@
 import type { Request, Response } from 'express'
 import * as authService from '../services/authServices'
-import type { loginDTO, registerDTO } from '../interfaces/auth'
-import jwt from 'jsonwebtoken'
+import type {
+  changePasswordDTO,
+  loginDTO,
+  registerDTO,
+  refreshDTO
+} from '../interfaces/auth'
 
 export const register = async (_req: Request, res: Response) => {
   try {
@@ -20,10 +24,6 @@ export const register = async (_req: Request, res: Response) => {
 
 export const login = async (_req: Request, res: Response) => {
   try {
-    const secretKey = process.env.JWT_SECRET
-    if (!secretKey) {
-      throw new Error('Falta configurar la variable de entorno JWT_SECRET')
-    }
     const data: loginDTO = res.locals.validateBody
 
     const result = await authService.loginServices(data)
@@ -32,18 +32,10 @@ export const login = async (_req: Request, res: Response) => {
       return res.status(400).json({ message: result.message })
     }
 
-    const token = jwt.sign(
-      {
-        id: result.data.id,
-        superadmin: result.data.superadmin
-      },
-      secretKey,
-      { expiresIn: '1d' }
-    )
-    console.log('La result: ', result)
     return res.status(200).json({
       message: result.message,
-      token,
+      accessToken: result.data.accessToken,
+      refreshToken: result.data.refreshToken,
       user: {
         id: result.data.id,
         email: result.data.email,
@@ -51,8 +43,48 @@ export const login = async (_req: Request, res: Response) => {
         lastName: result.data.lastName,
         phone: result.data.phone,
         superadmin: result.data.superadmin,
-        stores: result.data.stores // para el menu desplegable
+        stores: result.data.stores
       }
+    })
+  } catch (error) {
+    return res.status(500).json({ message: 'Error interno del servidor' })
+  }
+}
+
+export const changePassword = async (_req: Request, res: Response) => {
+  try {
+    const secretKey = process.env.JWT_SECRET
+
+    if (!secretKey) {
+      throw new Error('Falta configurar la variable de entorno JWT_SECRET')
+    }
+
+    const data: changePasswordDTO = res.locals.validateBody
+
+    const result = await authService.changePasswordService(data)
+
+    if (!result.success || !result.data) {
+      return res.status(400).json({ message: result.message })
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'Error interno del servidor' })
+  }
+}
+
+export const refresh = async (_req: Request, res: Response) => {
+  try {
+    const data: refreshDTO = res.locals.validateBody
+
+    const result = await authService.refreshTokenService(data)
+
+    if (!result.success || !result.data) {
+      return res.status(401).json({ message: result.message })
+    }
+
+    return res.status(200).json({
+      message: result.message,
+      accessToken: result.data.accessToken,
+      refreshToken: result.data.refreshToken
     })
   } catch (error) {
     return res.status(500).json({ message: 'Error interno del servidor' })
